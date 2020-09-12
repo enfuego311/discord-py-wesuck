32ae16024c3342bcc6e0b417feb43dfbbbd7bccd
enfuego311 <greg311@gmail.com>
2020-09-12 12:46:31 -0700

    unstuck & working sgif

--- discord-wesuck.py
+++ discord-wesuck.py
@@ -1,16 +1,20 @@
 import os
 import sys
 import discord
+import json
+import aiohttp
+import asyncio
 from discord.ext import commands
 import random
 
 token = os.environ.get("DISCORD_TOKEN")
+tenorapi = os.environ.get("TENOR_API_KEY")
 
 description = '''An example bot to showcase the discord.ext.commands extension
 module.
 
 There are a number of utility commands being showcased here.'''
-bot = commands.Bot(command_prefix='?', description=description)
+bot = commands.Bot(command_prefix='', description=description)
 
 def random_line(fname):
     lines = open(fname).read().splitlines()
@@ -23,72 +27,38 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
+@bot.command(pass_context=True)
+async def sgif(ctx, *, search):
+    confilter = "low"
+    embed = discord.Embed(colour=discord.Colour.blue())
+    session = aiohttp.ClientSession()
+    search.replace(' ', '+')
+    response = await session.get('http://api.tenor.com/v1/search?key=' + tenorapi + '&q=' + search + '&contentfilter=' + confilter + '&limit=1&media_filter=minimal')
+    data = json.loads(await response.text())
+    embed.set_image(url=data['results'][0]['media'][0]['gif']['url'])
+    
+    await session.close()
+
+    await ctx.send(embed=embed)
+
 @bot.event
 async def on_message(message):
     string1 = "marcus"
     string2 = "movie night"
     string3 = "herzog"
-    
+
     if string1.lower() in message.content.lower():
         channel = message.channel
         await channel.send(random_line(os.path.join(sys.path[0], 'marcus.txt')))
 
-    
     if string2.lower() in message.content.lower():
         channel = message.channel
         await channel.send(random_line(os.path.join(sys.path[0], 'movienight.txt')))
 
-    
     if string3.lower() in message.content.lower():
         channel = message.channel
         await channel.send(random_line(os.path.join(sys.path[0], 'herzog.txt')))
 
-
-@bot.command()
-async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left + right)
-
-@bot.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
-
-@bot.command(description='For when you wanna settle the score some other way')
-async def choose(ctx, *choices: str):
-    """Chooses between multiple choices."""
-    await ctx.send(random.choice(choices))
-
-@bot.command()
-async def repeat(ctx, times: int, content='repeating...'):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await ctx.send(content)
-
-@bot.command()
-async def joined(ctx, member: discord.Member):
-    """Says when a member joined."""
-    await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
-
-@bot.group()
-async def cool(ctx):
-    """Says if a user is cool.
-
-    In reality this just checks if a subcommand is being invoked.
-    """
-    if ctx.invoked_subcommand is None:
-        await ctx.send('No, {0.subcommand_passed} is not cool'.format(ctx))
-
-@cool.command(name='bot')
-async def _bot(ctx):
-    """Is the bot cool?"""
-    await ctx.send('Yes, the bot is cool.')
+    await bot.process_commands(message)
 
 bot.run(token)

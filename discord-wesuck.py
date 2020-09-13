7fcb4a4ba5479710a13c8e3fa3de7e3617e63c0e
enfuego311 <greg311@gmail.com>
2020-09-12 19:01:29 -0700

    attempt at adding weather

--- discord-wesuck.py
+++ discord-wesuck.py
@@ -9,6 +9,8 @@ import random
 
 token = os.environ.get("DISCORD_TOKEN")
 tenorapi = os.environ.get("TENOR_API_KEY")
+weatherapi = os.environ.get("WEATHER_API_KEY")
+googleapi = os.environ.get("GOOGLE_API_KEY")
 
 description = '''An example bot to showcase the discord.ext.commands extension
 module.
@@ -55,12 +57,38 @@ async def gif(ctx, *, search):
 
     await ctx.send(embed=embed)
 
+
+@bot.command(pass_context=True)
+async def weather(ctx, *, search):
+    session = aiohttp.ClientSession()
+    search.replace(' ', '+')
+    georesp = await session.get('https://maps.googleapis.com/maps/api/geocode/json?key=' + googleapi + '&address=' + search, ssl=False)
+    geodata = json.loads(await georesp.text())
+    geolat = geodata['results'][0]['geometry']['location']['lat']
+    geolon = geodata['results'][0]['geometry']['location']['lng']
+    await session.close()
+    weatheresp = await session.get('http://api.openweathermap.org/data/2.5/weather?appid=' + weatherapi + '&lat=' + geolat + '&lon=' + geolon + '&units=imperial')
+    locmapurl = 'https://maps.googleapis.com/maps/api/staticmap?center=' + geolat + ',' + geolon + '&zoom=11&size=600x200&key=' + googleapi
+    temp = weatheresp['main']['temp']
+    city = weatheresp['name']
+    country = weatheresp['sys']['country']
+    cond = weatheresp['weather'][0]['description']
+    feelslike = weatheresp['main']['feels_like']
+    humidity = weatheresp['main']['humidity']
+    await session.close()
+    embed = discord.Embed(title="Weather for" + city + " " + country, colour=discord.Colour.blue())
+    embed.set_image(url=locmapurl)
+    embed.add_field(name="Current Temp (feelslike)", value=temp + "F" + " " + "(" + feelslike + "F)")
+    embed.add_field(name="Conditions", value=cond)
+    embed.add_field(name="Humidity", value=humidity + "%")
+
+    await ctx.send(embed=embed)
+
 @bot.event
 async def on_message(message):
     namestr = "marcus"
     moviestr = "movie night"
     herzogstr = "herzog"
-
 
     if namestr.lower() in message.content.lower():
         channel = message.channel

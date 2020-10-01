4a08375918b101953d8ee4f89a12ca2fc55508ec
Gregory Rosenbaum <grosenbaum@ffn.com>
2020-10-01 09:48:04 -0700

    adding botmention

--- discord-wesuck.py
+++ discord-wesuck.py
@@ -28,6 +28,8 @@ def random_line(fname):
     lines = open(fname).read().splitlines()
     return random.choice(lines)
 
+
+
 # if we see this the bot is alive
 @bot.event
 async def on_ready():
@@ -268,62 +270,60 @@ async def on_message(message):
     neatostr = "neato"
     zeldastr = "zelda"
     bofhstr = "error"
+    # this is the *real* bot username - not the nickname
+    botstr = bot.user.name
+
+    # what channel is this? needed for channel.send
+    channel = message.channel
     
     # bot ignores botself
     if message.author.id == bot.user.id:
             return
 
+    # reacts to namestr if not botstr
+    if (namestr.lower() in message.content.lower()) and (botstr.lower() not in message.content.lower()):
+        await channel.send(random_line(os.path.join(sys.path[0], 'name.txt')))
+    elif botstr.lower() in message.content.lower():
+        line = random_line(os.path.join(sys.path[0], 'botmention.txt'))
+        response = line.replace("BOT", botstr)
+        await channel.send(response)
+    
     # wotd reaction
     if swotd.lower() in message.content.lower():
         await wotdreact(message)
 
     # other word matches with static and random line responses below
-    if namestr.lower() in message.content.lower():
-        channel = message.channel
-        await channel.send(random_line(os.path.join(sys.path[0], 'name.txt')))
-
     if moviestr.lower() in message.content.lower():
-        channel = message.channel
         await channel.send(random_line(os.path.join(sys.path[0], 'movienight.txt')))
 
     if herzogstr.lower() in message.content.lower():
-        channel = message.channel
         await channel.send(random_line(os.path.join(sys.path[0], 'herzog.txt')))
 
     if bofhstr.lower() in message.content.lower():
-        channel = message.channel
         await channel.send(random_line(os.path.join(sys.path[0], 'bofh.txt')))
     
     if herstr.lower() in message.content.lower():
-        channel = message.channel
         await channel.send("At least until operation: kill wife and kids")
 
     if kartstr.lower() in message.content.lower():
-        channel = message.channel
         await channel.send("**Official We Suck Mario Kart Ranking** - 8 > SNES > 64 > 7 > Wii > GBA > DS > DD")
     
     if mariostr.lower() in message.content.lower():
-        channel = message.channel
         await channel.send("**Official We Suck Mario Ranking** - Odyssey > 64 > World 2 > 3 > World > 3D World > Galaxy > 1 > 2 > Galaxy 2 > Sunshine")
     
     if ff2str.lower() in message.content.lower():
-        channel = message.channel
         await channel.send("The thing about civilization is that we are all 72 hours away from pure cannibalistic anarchy. That clock gets reset everytime we eat, everytiem we sleep but all of life as know it are on a precipice. FF2 was about 48 hrs for me. Everything you know and care about means nothing. That's the reality of culture and civilzation. It's an absolute cosmic shadow held up by essentially nothing. Final fantasy 2 taught me that.")
     
     if typongstr.lower() in message.content.lower():
-       channel = message.channel
        await channel.send("Don't make fun of my typong")
     
     if ffstr.lower() in message.content.lower():
-        channel = message.channel
         await channel.send("**Official We Suck Final Fantasy Ranking** - FF6 > FF4 > FF7 > FF9 > FF15 > FF10 > FF12 > FF1 > FF5 > FF8 > FF3 > FF13 > FF2")
 
     if neatostr.lower() in message.content.lower():
-       channel = message.channel
        await channel.send("neato burrito")
 
     if zeldastr.lower() in message.content.lower():
-       channel = message.channel
        await channel.send("**Official We Suck Zelda Ranking** - BotW > LttP > LBW > OoT > WW > LoZ > LA > TP > MM > AoL > SS")
     
     # this keeps us from getting stuck in this function
